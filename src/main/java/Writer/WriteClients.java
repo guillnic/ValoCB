dabce89601bd4628fc0fc39eded9cd1e4f766537
guillnic <>
2022-06-17 10:18:32 +0200

    Factorisation writers

--- src/main/java/Writer/WriteClients.java
+++ src/main/java/Writer/WriteClients.java
@@ -1,2 +1,32 @@
-package Writer;public class WriteClients {
+package Writer;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class WriteClients {
+    private WriteClients() {
+        throw new AssertionError();
+    }
+
+    public static void writeClient(String fileName, HashMap<String, Float> clientsCapital) throws IOException {
+        /**
+         * @param fileName : The name of the output file
+         * @param clientsCapital : The data that maps the clients with their capital
+         * @throws IOException
+         * This function writes the results of the processing of the capital of the clients in a csv file
+         * **/
+        CSVWriter writer = new CSVWriter(new FileWriter(fileName, false), ',', CSVWriter.NO_QUOTE_CHARACTER);
+        String header = "Client.Client,capital";
+        writer.writeNext(header);
+
+        for (var entry : clientsCapital.entrySet()) {
+            String[] nextLine = (entry.getKey() + "," + entry.getValue()).split(",");
+            writer.writeNext(nextLine);
+        }
+        writer.close();
+    }
+
 }
--- src/main/java/Writer/WritePortfolios.java
+++ src/main/java/Writer/WritePortfolios.java
@@ -1,2 +1,32 @@
-package Writer;public class WritePortfolios {
+package Writer;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class WritePortfolios {
+    // Non-instantiable class
+    private WritePortfolios() {
+        throw new AssertionError();
+    }
+
+    public static void writePortfolios(String fileName, HashMap<String, Float> portfolioPrice) throws IOException {
+        /**
+         * @param fileName : The name of the output file
+         * @param portfolioPrice : The data that maps the portfolios with their price
+         * @throws IOException
+         * This function writes the results of the processing of the value of the portfolio in a csv file
+         * **/
+        CSVWriter writer = new CSVWriter(new FileWriter(fileName, false), ',', CSVWriter.NO_QUOTE_CHARACTER);
+        String header = "PTF,price";
+        writer.writeNext(header);
+
+        for (var entry : portfolioPrice.entrySet()) {
+            String[] nextLine = (entry.getKey() + "," + entry.getValue()).split(",");
+            writer.writeNext(nextLine);
+        }
+        writer.close();
+    }
 }
